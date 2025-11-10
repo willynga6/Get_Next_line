990eddfefa14d4c0c89bd87edabe43aaa07c16af
Willynga6 <ton_email_github@exemple.com>
2025-11-10 02:32:43 +0100

    Correction header

--- GNL_BONUS/get_next_line_bonus.h
+++ GNL_BONUS/get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wngambi <wngambi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 11:50:55 by wngambi           #+#    #+#             */
-/*   Updated: 2025/11/08 18:18:21 by wngambi          ###   ########.fr       */
+/*   Updated: 2025/11/10 02:31:59 by wngambi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
